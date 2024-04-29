0d37cbfe82db90d3fc5a1d4a008872cbf288dd00
marusho26 <marusho106msy@gmail.com>
2024-04-29 20:22:52 +0900

    step07 study

--- __my/test.py
+++ __my/test.py
@@ -9,10 +9,16 @@ x = s.Variable(np.array(10))
 a = A(x)
 b = B(a)
 y = C(b)
-print(y.data)
 
-y.grad = np.array(1.0)
-b.grad = C.backward(y.grad)
-a.grad = B.backward(b.grad)
-x.grad = A.backward(a.grad)
-print(f"x.data:{x.data}/x.grad{x.grad}")
+# y.grad = np.array(1.0)
+# b.grad = C.backward(y.grad)
+# a.grad = B.backward(b.grad)
+# x.grad = A.backward(a.grad)
+# print(f"x.data:{x.data}/x.grad{x.grad}")
+
+assert y.creator == C
+assert y.creator.input == b
+assert y.creator.input.creator == B
+assert y.creator.input.creator.input == a
+assert y.creator.input.creator.input.creator == A
+assert y.creator.input.creator.input.creator.input == x
