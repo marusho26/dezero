67596781698c174d2d777919d71f4a577f86873a
marusho26 <marusho106msy@gmail.com>
2024-05-01 15:19:17 +0900

    add new study

--- __my/steps/step09.py
+++ __my/steps/step09.py
@@ -2,6 +2,9 @@ import numpy as np
 
 class Variable:
     def __init__(self, data):
+        if data is not None:
+            if not isinstance(data, np.ndarray):
+                raise TypeError('{}is not supported'.format(type(data)))
         self.data = data
         self.grad = None
         self.creator = None
@@ -9,17 +12,23 @@ class Variable:
         self.creator = func
 
     def backward(self):
-        f = self.creator
-        if f is not None:
-            x = f.input
-            x.grad = f.backward(self.grad)
-            x.backward()
+        if self.grad is None:
+            self.grad = np.ones_like(self.data)
+
+        funcs = [self.creator]
+        while funcs:
+            f = funcs.pop()
+            x, y = f.input, f.output
+            x.grad = f.backward(y.grad)
+
+            if x.creator is not None:
+                funcs.append(x.creator)
 
 class Function:
     def __call__(self, input):
         x = input.data
         y = self.forward(x) # foward関数は、変化させたい＆その関数の呼び出しはクラス内で行われる
-        output = Variable(y)
+        output = Variable(as_array(y))
         output.set_creator(self)
         self.input = input
         self.output = output
@@ -50,3 +59,8 @@ class Exp(Function):
         x = self.input.data
         gx = np.exp(x) * gy
         return gx
+    
+def as_array(x):
+    if np.isscalar(x):
+        return np.array(x)
+    return x
